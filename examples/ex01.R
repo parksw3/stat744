a38d007fd4087089e40b03245fd586de35faa326
parksw3 <parksw3@mcmaster.ca>
2018-01-09 14:14:36 -0500

    time for topology

---
+++ examples/ex01.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(ggplot2); theme_set(theme_bw())
+library(readr)
+
+cdat <- read_csv("../data/Carbohydrate_diet.csv")
+## print(cdat, n=8)
+
+gg0 <- (ggplot(cdat, 
+               aes(weight, carbohydrate))
+        + geom_point()
+)
+
+print(gg0)
+print(gg0 + geom_smooth(method="loess")
+          + geom_smooth(method="lm", col="red"))
+
+cdat <- mutate(cdat, f_age=cut_number(age,3))
