941cb95d82e6b077e9aa778b5cad72783db91016
parksw3 <parksw3@mcmaster.ca>
2018-01-13 03:07:57 -0500

    should I sleep?

--- HW1/HW1.R
+++ HW1/HW1.R
@@ -1,19 +1,28 @@
 library(tidyverse)
-library(ggplot2); theme_set(theme_bw())
-library(lme4)
-library(tsiR)
+library(ggplot2); theme_set(theme_bw(base_size = 12,
+                                     base_family = "Times"))
 library(geosphere)
-library(emdbook)
+library(ggmap)
+library(gridExtra)
+
+if (.Platform$OS.type=="windows") {
+    windowsFonts(Times=windowsFont("Times"))
+} 
+
+save <- FALSE
+
+scale_colour_discrete <- function(...,palette="Dark2") scale_colour_brewer(...,palette=palette)
+scale_fill_discrete <- function(...,palette="Dark2") scale_fill_brewer(...,palette=palette)
 
 ## http://datadryad.org/resource/doi:10.5061/dryad.r4q34
-
 df <- read.csv("../data/measlesUKUS.csv")
 
 transdf <- df %>% mutate(cases=ifelse(is.na(cases), 0, cases)) %>%
-    rename(births=rec, time=decimalYear)
+    rename(births=rec, time=decimalYear) %>%
+    filter(loc != "READING.US")
 
 sumdf <- transdf %>%
-    group_by(loc, country) %>%
+    group_by(loc, country, lon, lat) %>%
     summarize(total=sum(cases))
 
 syncdf <- transdf %>%
@@ -25,7 +34,7 @@ syncdf <- transdf %>%
     ) %>%
     left_join(sumdf)
 
-city <- levels(syncdf$loc)
+city <- unique(syncdf$loc)
 reslist <- vector('list', length(city)*(length(city)-1)/2)
 for(i in 1:length(city)) {
     for(j in i:length(city)) {
@@ -60,7 +69,62 @@ gg_synchrony <- ggplot(synchrony, aes(dist/1000, synchrony, col=country)) +
         legend.position=c(0.1, 0.85)
     )
 
-ggsave("HW1_fig1.pdf", gg_synchrony, width=6, height=4)
+if (save) ggsave("HW1_fig1.pdf", gg_synchrony, width=6, height=4)
+
+uk <- map_data(map = "world", region = "UK") %>%
+    filter(!(subregion %in% c("Northern Ireland", "Scotland")), lat < 55.3)
+
+london_synchrony <- synchrony %>% 
+    filter(city1=="LONDON" | city2=="LONDON") %>%
+    select(-total, -country) %>%
+    group_by(city1, city2) %>%
+    mutate(
+        loc=if(city1=="LONDON") {city2} else {city1}
+    ) %>%
+    bind_rows(data.frame(
+        loc="LONDON",
+        synchrony=sum((unlist(filter(syncdf, loc=="LONDON")$normalized))^2)
+    )) %>%
+    left_join(sumdf, by="loc")
+
+london_minmax <- london_synchrony %>%
+    group_by %>%
+    filter(loc != "LONDON") %>%
+    summarize(
+        min=.[which.min(ccf),][["loc"]],
+        max=.[which.max(ccf),][["loc"]]
+    ) %>%
+    unlist
+
+london_line <- london_synchrony %>%
+    filter(loc %in% london_minmax) %>%
+    rename(target.lon=lon, target.lat=lat) %>%
+    mutate(lon=-0.105, lat=51.517)
+
+ggplot(london_synchrony, aes(lon, lat)) +
+    geom_polygon(data = uk, aes(x=long, y = lat, group = group), alpha=0.2) + 
+    geom_segment(data=london_line, aes(xend=target.lon, yend=target.lat, col=synchrony), 
+                 alpha=0.4, lwd=1.5, lty=2) +
+    geom_point(aes(size=total, col=synchrony), alpha=0.9) +
+    geom_text(data=london_line, aes(x=target.lon, y=target.lat, label=loc), vjust=2.8) +
+    geom_text(x=-0.105, y=51.517, label="LONDON", vjust=3.7) +
+    scale_size_continuous(range=c(3,20), guide=FALSE) +
+    scale_color_gradient(low="#56B4E9", high="#D55E00") +
+    theme(
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        legend.position = c(0.9, 0.8)
+    )
+
+ggplot(filter(transdf, loc %in% c(london_minmax)), aes(time, cases)) +
+    geom_line(data=filter(transdf, loc=="LONDON") %>% select(-loc), lty=2) +
+    geom_line(aes(col=loc)) +
+    scale_y_log10() +
+    facet_grid(loc~., scale="free")
+
 
 
 
